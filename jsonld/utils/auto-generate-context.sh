9da1b599d1f3ebc6800fbe5355cfe989fa5627af
Raul Palma <rapw2k@yahoo.com>
2023-10-16 18:02:51 +0200

    json-ld

--- jsonld/utils/auto-generate-context.sh
+++ jsonld/utils/auto-generate-context.sh
@@ -3,8 +3,8 @@
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o gdim-core-context.jsonld https://w3id.org/ad4gd/model/core
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o crossDomain-context.jsonld https://w3id.org/ad4gd/model/crossDomain
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o metadata-context.jsonld https://w3id.org/ad4gd/model/metadata
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o GDCommon-context.jsonld https://w3id.org/ad4gd/model/GDCommon
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o GDProperty-context.jsonld https://w3id.org/ad4gd/model/GDProperty
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o gdCommon-context.jsonld https://w3id.org/ad4gd/model/gdCommon
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o gdProperty-context.jsonld https://w3id.org/ad4gd/model/gdProperty
 
 
 ---generating extensions
@@ -20,3 +20,5 @@ java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld
 #      "@id" : "http://purl.org/dc/terms/description"
 #    }
 * save jsonld, commit and push    
+
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -i -a -o greenDealProfile-context.jsonld https://w3id.org/ad4gd/model
